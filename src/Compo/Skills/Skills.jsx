473d71a4a62380f4c5ffdae98f2f731f35212881
GabR3924 <garc2004@gmail.com>
2023-12-06 21:32:33 -0400

    dios

--- src/Compo/Skills/Skills.jsx
+++ src/Compo/Skills/Skills.jsx
@@ -1,6 +1,4 @@
 import './Skills.css'
-import { TbBrandRedux } from "react-icons/tb";
-import { SiAngular } from "react-icons/si";
 import React from '../../assets/react.png'
 import Js from '../../assets/js.png'
 import Css from '../../assets/css.png'
@@ -9,6 +7,14 @@ import Angular from '../../assets/angular.png'
 import Sql from '../../assets/sql.png'
 import Selenium from '../../assets/selenium.png'
 import Mongo from '../../assets/mongo.png'
+import Java from '../../assets/java.png'
+import Post from '../../assets/post.png'
+import Node from '../../assets/node.png'
+import Express from '../../assets/express.png'
+import Aws from '../../assets/aws.png'
+import Git from '../../assets/git.png'
+import Postman from '../../assets/postman.png'
+
 
 function Skills() {
   return (
@@ -33,7 +39,7 @@ function Skills() {
                 </div>
             </div >
             <div className='sks'>
-                <div className="add"></div>
+                <div className="add">+</div>
                 <img src={Js} alt="" />
                 <img src={Css} alt="" />
                 <img src={Figma} alt="" />
@@ -42,6 +48,13 @@ function Skills() {
                 <img src={Sql} alt="" />
                 <img src={Selenium} alt="" />
                 <img src={Mongo} alt="" />
+                <img src={Java} alt="" />
+                <img src={Post} alt="" />
+                <img src={Node} alt="" />
+                <img src={Express} alt="" />
+                <img src={Git} alt="" />
+                <img src={Aws} alt="" />
+                <img src={Postman} alt="" />
             </div>
             <div className='about'> 
                 <h3>about</h3>
