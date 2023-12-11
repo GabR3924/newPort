1b178b9801e99a35e274cb0d417c0a0636822e28
GabR3924 <garc2004@gmail.com>
2023-12-11 15:01:20 -0400

    ok

--- src/Compo/Projects/Projects.jsx
+++ src/Compo/Projects/Projects.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import Template from '../../assets/Desktop.png';
 import './Projects.css';
+import { Data } from '../../data.js';
 
 function Projects() {
-  // Utilizamos un objeto para almacenar el estado de cada contenedor
   const [detailVisible, setDetailVisible] = useState({
     uno: false,
     dos: false,
@@ -12,103 +11,38 @@ function Projects() {
   });
 
   const toggleDetail = (container) => {
-    // Creamos una copia del estado actual
     const updatedDetailVisible = { ...detailVisible };
-    // Cambiamos el estado del contenedor específico
     updatedDetailVisible[container] = !updatedDetailVisible[container];
-    // Actualizamos el estado global
     setDetailVisible(updatedDetailVisible);
 
-    // Ajusta la clase del contenedor específico
     const containerElement = document.querySelector(`.${container}`);
     containerElement.classList.toggle('expanded');
   };
 
   return (
     <section className='section'>
-      <h1>Projectos</h1>
-      {/* <div className='options'>
-        <h1>Web App's</h1>
-        <h1>Java</h1>
-        <h1>API's</h1>
-        <h1>UX UI</h1>
-      </div> */}
+      <h1>Proyectos</h1>
       <div className="projects">
-        <div className='uno'>
-          <div className='img'></div>
-          <div className="info">
-            <h3>NFTs Website</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
-            <ul>
-              <li>UX/UI</li>
-              <li>Frontend</li>
-              <li>CSS</li>
-            </ul>
-            <button className='ver' onClick={() => toggleDetail('uno')}>
-              Ver
-            </button>
+        {Data.map((project) => (
+          <div key={project.id} className={project.id}>
+            <img src={project.image} alt={project.title} className='img' />
+            <div className="info">
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <ul>
+                {project.categories.map((category, index) => (
+                  <li key={index}>{category}</li>
+                ))}
+              </ul>
+              <button className='ver' onClick={() => toggleDetail(project.id)}>
+                Ver
+              </button>
+            </div>
+            <div className={`detail ${detailVisible[project.id] ? 'visible' : ''}`}>
+              {/* Contenido del detalle del contenedor */}
+            </div>
           </div>
-          <div className={`detail ${detailVisible.uno ? 'visible' : ''}`}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fugiat autem quia. Ducimus maxime exercitationem aliquid debitis saepe. Laborum quidem et ipsum labore fugit pariatur perferendis, ut debitis a soluta.
-            Quaerat porro tempora, ad dolorem tempore odio? Quidem explicabo iure quibusdam, voluptates aliquid at eum, nisi numquam, sit necessitatibus modi facere illo non! Ducimus, est fuga ipsa ipsam iure perferendis.
-            Natus harum eos delectus alias! Adipisci alias molestias ad autem minus in perspiciatis repellendus odit inventore quisquam reprehenderit, praesentium, harum nihil, culpa expedita omnis. Distinctio pariatur tenetur impedit aperiam voluptatibus.
-            Necessitatibus hic esse ab, veniam quod, veritatis eos molestias amet iste eius magni! Recusandae ullam similique nostrum commodi asperiores, fugiat amet laboriosam veritatis. Earum saepe consequuntur consequatur fugit atque maxime?
-          </div>
-        </div>
-        <div className='dos'>
-          <div className='img'></div>
-          <div className="info">
-            <h3>NFTs Website</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
-            <ul>
-              <li>UX/UI</li>
-              <li>Frontend</li>
-              <li>CSS</li>
-            </ul>
-            <button className='ver' onClick={() => toggleDetail('dos')}>
-              Ver
-            </button>
-          </div>
-          <div className={`detail ${detailVisible.dos ? 'visible' : ''}`}>
-            {/* Contenido del detalle del contenedor dos */}
-          </div>
-        </div>
-        <div className='tres'>
-          <div className='img'></div>
-          <div className="info">
-            <h3>NFTs Website</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
-            <ul>
-              <li>UX/UI</li>
-              <li>Frontend</li>
-              <li>CSS</li>
-            </ul>
-            <button className='ver' onClick={() => toggleDetail('tres')}>
-              Ver
-            </button>
-          </div>
-          <div className={`detail ${detailVisible.tres ? 'visible' : ''}`}>
-            {/* Contenido del detalle del contenedor tres */}
-          </div>
-        </div>
-        <div className='cuatro'>
-          <div className='img'></div>
-          <div className="info">
-            <h3>NFTs Website</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
-            <ul>
-              <li>UX/UI</li>
-              <li>Frontend</li>
-              <li>CSS</li>
-            </ul>
-            <button className='ver' onClick={() => toggleDetail('cuatro')}>
-              Ver
-            </button>
-          </div>
-          <div className={`detail ${detailVisible.cuatro ? 'visible' : ''}`}>
-            {/* Contenido del detalle del contenedor cuatro */}
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const Data = [
+    {
+      id: 'uno',
+      title: 'NFTs Website',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+      categories: ['UX/UI', 'Frontend', 'CSS'],
+      image: './src/assets/Desktop.png', // Ruta a tu imagen
+    },
+    {
+        id: 'dos',
+        title: 'NFTs Website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+        categories: ['UX/UI', 'Frontend', 'CSS'],
+        image: './src/assets/Desktop.png', // Ruta a tu imagen
+    },
+    {
+        id: 'tres',
+        title: 'NFTs Website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+        categories: ['UX/UI', 'Frontend', 'CSS'],
+        image: './src/assets/Desktop.png', // Ruta a tu imagen
+    },
+    {
+        id: 'cuatro',
+        title: 'NFTs Website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+        categories: ['UX/UI', 'Frontend', 'CSS'],
+        image: './src/assets/Desktop.png', // Ruta a tu imagen
+    },
+  ];
+  
